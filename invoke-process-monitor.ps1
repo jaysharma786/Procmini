81e86e0745144a6ee9758fd5082b6f7c32f14b4e
Jay Sharma <46397588+jaysharma786@users.noreply.github.com>
2023-07-15 14:59:31 +0530

    Powershell script file

---
+++ invoke-process-monitor.ps1
@@ -0,0 +1,39 @@
+﻿# Function to continuously monitor and dump process information
+function Monitor-Process {
+    param (
+        [string]$ProcessName
+    )
+
+    # Retrieve the process ID for the specified process name
+    $processID = (Get-Process -Name $ProcessName).Id
+
+    if ($processID -eq $null) {
+        Write-Host "Process '$ProcessName' not found."
+        return
+    }
+
+    Write-Host "Monitoring process '$ProcessName' (ID: $processID). Press Ctrl+C to stop."
+
+    # Infinite loop to continuously monitor the process
+    while ($true) {
+        try {
+            # Retrieve process information
+            $process = Get-Process -Id $processID
+
+            # Dump process information
+            $process | Format-List * | Out-File -FilePath "ProcessDump_$processID.txt" -Force -Append
+
+            # Delay for 1 second before next iteration
+            Start-Sleep -Seconds 1
+        }
+        catch {
+            # Process no longer exists, exit the loop
+            Write-Host "Process '$ProcessName' (ID: $processID) no longer exists."
+            break
+        }
+    }
+
+    Write-Host "Process monitoring stopped."
+}
+
+# Usage example: Monitor-Process -ProcessName "notepad"
